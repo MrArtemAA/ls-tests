fbe97e1123edb85200b0e3a0a2bf190ba579dcd7
Artem Areshko <artemaa@gmail.com>
2018-02-11 23:53:22 +0300

    Added getting currency by code

--- src/main/java/ru/livescripts/tests/javamoney/web/CurrencyRestController.java
+++ src/main/java/ru/livescripts/tests/javamoney/web/CurrencyRestController.java
@@ -1,7 +1,8 @@
 package ru.livescripts.tests.javamoney.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ru.livescripts.tests.javamoney.dto.Currency;
 
@@ -11,27 +12,36 @@ import java.util.Collection;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
 @RestController
-@RequestMapping(path = "api/v1/currencies", method = GET)
+@RequestMapping("/api/v1/currencies")
 public class CurrencyRestController {
 
+    @GetMapping
     public Iterable<Currency> getAll() {
         Collection<CurrencyUnit> currenciesByDefaultProvider = Monetary.getCurrencies("default");
         Locale locale = Locale.getDefault();
         return currenciesByDefaultProvider.stream()
-                .map(currencyUnit -> {
-                    String currencyCode = currencyUnit.getCurrencyCode();
-                    java.util.Currency javaCurrency = java.util.Currency.getInstance(currencyCode);
-                    String currencyDisplayName = javaCurrency.getDisplayName(locale);
-                    Currency currency = new Currency();
-                    currency.name = currencyDisplayName;
-                    currency.code = currencyCode;
-                    return currency;
-                })
+                .map(this::getCurrency)
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{code}")
+    public Currency getByCode(@PathVariable("code") String code) {
+        CurrencyUnit currencyUnit = Monetary.getCurrency(code.toUpperCase(), "default");
+        return getCurrency(currencyUnit);
+    }
+
+    private Currency getCurrency(CurrencyUnit currencyUnit) {
+        String currencyCode = currencyUnit.getCurrencyCode();
+        Currency currency = new Currency();
+        currency.name = getCurrencyDisplayName(currencyCode);
+        currency.code = currencyCode;
+        return currency;
+    }
+
+    private String getCurrencyDisplayName(String currencyCode) {
+        java.util.Currency javaCurrency = java.util.Currency.getInstance(currencyCode);
+        return javaCurrency.getDisplayName(Locale.getDefault());
     }
 
 }
